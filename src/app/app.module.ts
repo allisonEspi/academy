88e88e7d37616d04fb37e8566eb280266ee9f84f
Allison Espinoza <alleespi@espol.edu.ec>
2020-11-02 01:06:41 -0500

    capacitor camera preview

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { DeviceMotion} from '@ionic-native/device-motion/ngx';
+import { DeviceOrientation } from '@ionic-native/device-orientation/ngx';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Geolocation,
+    DeviceMotion,
+    DeviceOrientation,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
